36a9f57d1a7dc567b1ff53e67f226bba9679675d
Mohammed Kamil Nyaba Abdul-Razak <168296043+CodeMaesttro@users.noreply.github.com>
2025-07-13 16:22:55 -0700

    Update ConfirmModal.jsx

--- frontend/src/components/ConfirmModal.jsx
+++ frontend/src/components/ConfirmModal.jsx
@@ -15,13 +15,13 @@ function ConfirmModal({ setShowModal, productId, refreshProducts }) {
       const data = await response.json();
 
       if (response.ok) {
-        toast.success("🗑️ Product deleted successfully!");
+        toast.success("Product deleted successfully!");
         setShowModal(false); // Close modal quickly
         refreshProducts(); // Then refresh product list
       }
     } catch (error) {
       console.error(error);
-      toast.error("❌ Failed to delete product. Try again.");
+      toast.error(" Failed to delete product. Try again.");
     }
   }
 
@@ -29,7 +29,7 @@ function ConfirmModal({ setShowModal, productId, refreshProducts }) {
     <div className="absolute w-full h-screen bg-black/60 top-0 left-0 z-50 flex justify-center items-center">
       <div className="max-w-2xl mx-auto bg-gray-900 border border-gray-500/25 p-4 rounded">
         <h2 className="text-white text-lg font-bold mb-4">
-          Confirm Product Deletion
+          Confirm Product Deletion!
         </h2>
 
         <div className="flex gap-2">
